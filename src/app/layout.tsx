179649a3103c1b196d3a9730022c568a4b6b3f69
devshahorair <shuvo1556@gmail.com>
2025-07-06 21:24:26 +0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
           name='googlebot-news'
           content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
         />
-        <Script src='https://www.googletagmanager.com/gtag/js?id=G-6HG3ZJWVCP'></Script>
-        <Script>
+        <Script src='https://www.googletagmanager.com/gtag/js?id=G-6HG3ZJWVCP' strategy='lazyOnload'></Script>
+        <Script strategy='lazyOnload'>
           {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
